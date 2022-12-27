8d201a8f6f58853bba569cc6de0471956b5c5665
DummyWoke <ramseybanda2@gmail.com>
2022-12-27 04:14:18 -0500

    final

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var ScoreEl = document.getElementById("ScoreButton")
+var Time = document.getElementById("Time")
+var QuizMain = document.getElementById("Quiz")
+var StartButton = document.getElementById("StartButton")
+var QuestionScreen = document.getElementById("QuestionScreen")
+var Questiontitle = document.getElementById("Questiontitle")
+
+//Setup Listeners
+
+QuizStartButton.onclick = () => {
+
+console.log("hi")
+document.querySelector(Quiz).style.display = hidden
+}
+
+ScoreButton.onclick = () => {
+
+    console.log("Bye")
+    }
